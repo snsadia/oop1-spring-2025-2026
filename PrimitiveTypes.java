2037bdcdc89b59f3dab49ffa8c3dbdb4b9d8bfbb
snsadia <snsadiachowdhury@gmail.com>
2026-02-25 22:30:42 +0600

    first commit

---
+++ PrimitiveTypes.java
@@ -0,0 +1,26 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte age = 21;
+        short year = 2026;
+        int a = 20000;
+        long population = 8_887_000_000l;
+        float b = 6.20f;
+        double c = 15.953;
+        char d = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Byte: " + age);
+        System.out.println("Short: " + year);
+        System.out.println("Integer: " + a);
+        System.out.println("Long: " + population);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Character: " + d);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
+
+
+
+
+
